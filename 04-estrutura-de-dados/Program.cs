2ffe02a99232b21e48db633fc738270734ff7131
Arthur Tardin <arthurtgciongoli@gmail.com>
2026-08-18 19:46:23 -0300

    feat: add exercise

--- 04-estrutura-de-dados/Program.cs
+++ 04-estrutura-de-dados/Program.cs
@@ -1,1 +1,51 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+namespace Project04
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ListaDeCompras();
+        }
+
+        public static void ListaDeCompras()
+        {
+            List<string> compras = new List<string>{"Arroz", "Feijão", "Salada", "Carne"};
+            Console.WriteLine("Lista de compras: ");
+
+            Console.WriteLine("1 - Adicionar produto");
+            Console.WriteLine("2 - Remover produto");
+            Console.WriteLine("3 - Ver lista");
+
+            Console.Write("Escolha sua opção: ");
+            int.TryParse(Console.ReadLine(), out int choice);
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Digite o nome do produto:");
+                    string add = Console.ReadLine()!;
+
+                    compras.Add(add);
+                    break;
+                case 2:
+                    Console.WriteLine("Digite o nome do produto");
+                    string rm = Console.ReadLine()!;
+
+                    compras.Remove(rm);
+                    break;
+                case 3:
+                    foreach(string item in compras)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Escolha uma opção válida.");
+                    break;
+            }
+        }
+    }
+}
