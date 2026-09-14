4e040c73e934e4fd283d890aa73bc22ad615a851
Arthur Tardin <arthurtgciongoli@gmail.com>
2026-09-13 21:09:07 -0300

    feat: add tests and exercises

---
+++ 09-linq/teste/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using Project;
+
+public class UnitTest1
+{
+        [Fact]
+    public void NomesDosProdutosCaros_ComListaVazia_DeveRetornarListaVazia()
+    {
+        // Arrange
+        List<Produto> produtos = new List<Produto>();
+
+        // Act
+        List<string> resultado = FiltroProdutos.NomesDosProdutosCaros(produtos, 1000);
+
+        // Assert
+        Assert.Empty(resultado); // Assert.Empty confirma que a coleção não tem nenhum item
+    }
+}
